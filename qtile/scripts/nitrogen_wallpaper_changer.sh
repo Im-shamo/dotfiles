a0b0d8ecd4dd45ee9095ce94a4fe3a65e9b292fe
im-shamo <samuelkwok2007@gmail.com>
2024-06-29 17:49:25 +0800

    changed variable names

--- qtile/scripts/nitrogen_wallpaper_changer.sh
+++ qtile/scripts/nitrogen_wallpaper_changer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nitrogen --head=0 --set-zoom-fill --random
-nitrogen --head=1 --set-zoom-fill --random
+nitrogen --head=0 --set-zoom-fill --random >> /dev/null
+nitrogen --head=1 --set-zoom-fill --random >> /dev/null
