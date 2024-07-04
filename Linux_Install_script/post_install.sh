92ddf7aaf0e8a67cf0617178dc28cec5c9c8df4a
im-shamo <samuelkwok2007@gmail.com>
2024-07-04 23:54:38 +0800

    fix osu name

--- Linux_Install_script/post_install.sh
+++ Linux_Install_script/post_install.sh
@@ -1,3 +1,3 @@
 #/usr/bin/env bash
-~/Linux_scripts/gen-desktop.py "~/Application/osu.AppImage" "osu!"
+~/Linux_scripts/gen-desktop.py "~/Application/osu.AppImage" "osu"
 ~/Linux_scripts/gen-desktop.py "wine /home/shamokwok/Application/Foreman/Release/Foreman.exe" "Foreman"
