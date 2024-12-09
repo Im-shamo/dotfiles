34a0b6fe4171a249b9eda523ada126b5dbb8b585
Im-shamo <samuelkwok2007@gmail.com>
2024-12-09 17:10:24 +0800

    Update package_install.sh

--- Linux_Install/scripts/archlinux/package_install.sh
+++ Linux_Install/scripts/archlinux/package_install.sh
@@ -7,9 +7,8 @@ fi
 
 if ! command -v yay > 2 >&1 > /dev/null; then
     echo "installing yay"
-    temp=`mktemp`
-    cd $mktemp
-    
+    temp=`mktemp -d`
+    cd $temp
     git clone https://aur.archlinux.org/yay-bin.git
 
     cd yay-bin
@@ -20,25 +19,55 @@ sudo pacman -Syy
 
 # Desktop usage
 # Qtile
-
+echo "Qtile specific"
+sudo pacman -S --noconfirm --needed \
+    picom rofi pavucontrol nitrogen dbus libnotify xorg-server-xephyr
+cd $temp
+git clone https://aur.archlinux.org/qtile-extras.git
+cd qtile-extras
+makepkg -si --noconfirm
 
 # Hyprland
-
-
-# Office / Productivity
-
-
-# Theming
+sudo pacman -S --noconfirm --needed \
 
 
 # Utility
+echo "Installing Utility"
+sudo pacman -S --noconfirm --needed \
+    kitty alacarity \
+    wget curl vim udiskie \
+    flatpak \
+
+# Office / Productivity
+sudo pacman -S --noconfirm --needed \
+    libraoffice-fresh
 
 
-# Web
+# Theming
+sudo pacman -S --noconfirm --needed \
+    nwg-look breeze-gtk
 
+# Multimedia
+sudo pacman -S --noconfirm --needed \
+    firefox vlc 
 
 # Game
+sudo pacman -S --noconfirm --needed \
+    steam
 
+# Communication
+
+
+
+# Password
+flatpak --noninteractive install flathub org.keepassxc.KeePassXC
+
+
+# Fonts
+echo "installing fonts"
+sudo pacman -S --noconfirm --needed \
+    gnome-font-viewer \
+    noto-fonts noto-fonts-cjk noto-fonts-emoji otf-droid-nerd ttf-hack-nerd
 
 
 # 2. Development
@@ -52,3 +81,6 @@ sudo pacman -Syy
 
 
 # Rust development
+
+
+# 3. Virtualization
