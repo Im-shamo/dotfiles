0605590d2d04e74094945f4a114feb1e9683458b
Im-shamo <samuelkwok2007@gmail.com>
2024-12-11 17:57:33 +0800

    Add environment variable to qtile config

--- qtile/configuration/startup.py
+++ qtile/configuration/startup.py
@@ -12,3 +12,13 @@ if qtile.core.name == "x11":
 
         ### Environment Variables ###
         # TODO: add environment variables
+        os.environ["PATH"] = f"{os.path.expanduser("~/.local/bin")}:{os.environ["PATH"]}"
+        os.environ["EDITOR"] = "vim"
+        os.environ["VISUAL"] = "vim"
+        os.environ["BROWSER"] = browser
+
+        # hack to get qt apps to look correct
+        os.environ["QT_QPA_PLATFORMTHEME"] = "kde"
+
+        # ssh
+        os.environ["SSH_AUTH_SOCK"] = f"{os.path.join(os.environ["XDG_RUNTIME_DIR"], "gcr", "ssh")}"
