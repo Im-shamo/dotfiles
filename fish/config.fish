9e605b93e6e525d237ae03ebd4a8fc3a19bfaea6
Im-shamo <samuelkwok2007@gmail.com>
2024-12-17 20:26:35 +0800

    Change fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,2 @@
-if status is-interactive
-    if test "$TERM" = "xterm-kitty"
-        fastfetch --logo-width 35 --logo "$HOME/Clone/dotfiles/fish/images/firefly-3-nobg.png"
-    else
-        fastfetch
-    end
-end
 set fish_greeting
+fastfetch
