f3a72ce9a0e07814d918854021e06290b8658826
Im-shamo <samuelkwok2007@gmail.com>
2025-02-07 10:40:41 +0800

    Remove .vim, change qtile and waybar config

--- qtile/configuration/environment_varables.py
+++ qtile/configuration/environment_varables.py
@@ -5,11 +5,10 @@ from configuration.variables import *
 def set_environment_varables(backend = "x11"):
     os.environ["PATH"] = f"{os.path.expanduser("~/.local/bin")}:{os.path.expanduser("~/.config/rofi/applets/bin")}:{os.path.expanduser("~/.config/rofi/scripts")}:{os.environ["PATH"]}"
     os.environ["EDITOR"] = "vim"
-    os.environ["VISUAL"] = "vim"
     os.environ["BROWSER"] = browser
     
     # Theming
-    os.environ["QT_QPA_PLATFORMTHEME"] = "kde"
+    os.environ["QT_QPA_PLATFORMTHEME"] = "qt5ct"
     os.environ["XCURSOR_PATH"] = os.path.expanduser("~/.local/share/icons")
     # ssh
     os.environ["SSH_AUTH_SOCK"] = f"{os.path.join(os.environ["XDG_RUNTIME_DIR"], "gcr", "ssh")}"
