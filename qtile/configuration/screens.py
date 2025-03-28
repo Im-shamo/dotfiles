d9f7c83e11c9d2decc672a4adbe3999f7daeb7f1
Im-shamo <samuelkwok2007@gmail.com>
2025-03-28 23:07:48 +0800

    refactor: change how bar.py is called

--- qtile/configuration/screens.py
+++ qtile/configuration/screens.py
@@ -6,8 +6,7 @@ from configuration.bars import my_bars
 # TODO: Add wayland
 if qtile.core.name == "x11":
     screens = [
-        Screen(top = my_bars.secondary_bar_x11),
-        Screen(top = my_bars.main_bar_x11),
+        Screen(top = my_bars.main_bar_x11()),
     ]
 
 elif qtile.core.name == "wayland":
