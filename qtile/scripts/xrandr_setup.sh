851c12fbcaa1c6e4fb05267250b2e3ea8b0e205e
im-shamo <samuelkwok2007@gmail.com>
2025-09-26 09:52:17 +0800

    Update qtile xrandr script

--- qtile/scripts/xrandr_setup.sh
+++ qtile/scripts/xrandr_setup.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
-external=$(xrandr | grep -i  "HDMI" | cut -d " " -f1)
-laptop=$(xrandr | grep -i "eDP" | cut -d " " -f1)
+external=$(xrandr | grep -i  "HDMI" | cut -d " " -f1 | head -n 1)
+laptop=$(xrandr | grep -i "eDP" | cut -d " " -f1 | head -n 1)
+virtual=$(xrandr | grep -i "Virtual" | cut -d " " -f1 | head -n 1)
 rate=144
 res=1920x1080
 
-if [[ -z "$laptop" ]]; then
-xrandr --output "$external" --mode $res --rate $rate
+if [[ -n "$virtual" ]]; then
+echo "Set display: $virtual to $res@60"
+xrandr --output "$virtual" --mode "$res" --rate 60
+elif [[ -n "$laptop" ]]; then
+echo "Set display: $laptop to $res@$rate"
+xrandr --output "$laptop" --mode "$res" --rate $rate
+elif [[ -n "$laptop" ]] && [[ -n $external ]]; then
+echo "Set display: $laptop to $res@$rate $external to $res@$external"
+xrandr --output "$laptop" --mode  "$res" --rate $rate --output "$external" --mode $res --rate $rate --right-of "$laptop"
 else
-xrandr --output "$laptop" --mode  $res --rate $rate --output "$external" --mode $res --rate $rate --right-of "$laptop"
+echo "No display set"
 fi
