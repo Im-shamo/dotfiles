a7e8ec469057f020c8b3aaeab8bf33b61e026ff9
Im-shamo <samuelkwok2007@gmail.com>
2025-11-23 13:41:46 +0800

    Add prompt widget to qtile bars

--- qtile/configuration/bars.py
+++ qtile/configuration/bars.py
@@ -13,6 +13,7 @@ class MyBars:
                 widget.CurrentLayoutIcon(mouse_callbacks={"Button1": lazy.next_layout()}),
                 my_widgets.group_box(),
                 widget.WindowName(),
+                widget.Prompt(),
                 widget.Systray(),
                 widget.TextBox(**arrow_right),
                 my_widgets.wallpaper_switcher(**arrow_right),
@@ -29,7 +30,6 @@ class MyBars:
                 my_widgets.volume(),
                 my_widgets.microphone(**arrow_right),
                 widget.Clock(format="%d/%m/%Y %a %I:%M %p", background=colours["LIGHT_BLUE"], **arrow_right),   # cyan
-                widget.Battery(format="  {percent:.0%}",emoji=True,background=colours["BLUE"], **arrow_right),
                 my_widgets.power_button(),
                 widget.Spacer(length=5),
             ],
@@ -43,6 +43,7 @@ class MyBars:
                 widget.CurrentLayoutIcon(mouse_callbacks={"Button1": lazy.next_layout()}),
                 my_widgets.group_box(),
                 widget.WindowName(),
+                widget.Prompt(),
                 widget.Systray(),
                 widget.TextBox(**arrow_right),
                 my_widgets.wallpaper_switcher(**arrow_right),
@@ -67,42 +68,13 @@ class MyBars:
             opacity = 1,
             margin = bar_margin
         )
-    def main_bar_x11_desktop(self):
-        return bar.Bar(
-            [
-                widget.CurrentLayoutIcon(mouse_callbacks={"Button1": lazy.next_layout()}),
-                my_widgets.group_box(),
-                widget.WindowName(),
-                widget.Systray(),
-                widget.TextBox(**arrow_right),
-                my_widgets.wallpaper_switcher(**arrow_right),
-                widget.Net(font=mono_font, format= " {down:^5.1f}{down_suffix:<2}", background=colours["BLUE"],**arrow_right),  # blue
-                widget.Net(font=mono_font, format= " {up:^5.1f}{up_suffix:<2}", background=colours["DARK_GREEN"],**arrow_right),  # green
-                widget.NetGraph(
-                    mouse_callbacks={"Button1": lazy.spawn("networkmanager_dmenu")},
-                    background=colours["GREEN"],
-                    graph_color="215578",
-                    type="line",
-                    line_width=2,
-                    **arrow_right,
-                ),
-                my_widgets.volume(),
-                my_widgets.microphone(**arrow_right),
-                widget.Clock(format="%d/%m/%Y %a %I:%M %p", background=colours["LIGHT_BLUE"], **arrow_right),   # cyan
-                my_widgets.power_button(),
-                widget.Spacer(length=5),
-            ],
-            26,
-            opacity = 1,
-            margin = bar_margin
-        )
-
     def secondary_bar_x11(self):
         return bar.Bar(
             [
                 widget.CurrentLayoutIcon(),
                 my_widgets.group_box(),
                 widget.WindowName(),
+                widget.Prompt(),
                 widget.TextBox(**arrow_right),
                 widget.Clock(format="%d/%m/%Y %a %I:%M %p", background="1d6ac9", **arrow_right),   # Light blue
                 my_widgets.power_button(),
@@ -119,6 +91,7 @@ class MyBars:
                 widget.CurrentLayoutIcon(mouse_callbacks={"Button1": lazy.next_layout()}),
                 my_widgets.group_box(),
                 widget.WindowName(),
+                widget.Prompt(),
                 widget.Systray(),
                 widget.TextBox(**arrow_right),
                 my_widgets.wallpaper_switcher(**arrow_right),
@@ -139,7 +112,7 @@ class MyBars:
                 widget.CurrentLayoutIcon(mouse_callbacks={"Button1": lazy.next_layout()}),
                 my_widgets.group_box(),
                 widget.WindowName(),
-                widget.Notify(),
+                widget.Prompt(),
                 widget.Systray(),
                 widget.TextBox(**arrow_right),
                 my_widgets.volume(**arrow_right),
@@ -159,7 +132,7 @@ class MyBars:
                 widget.CurrentLayoutIcon(mouse_callbacks={"Button1": lazy.next_layout()}),
                 my_widgets.group_box(),
                 widget.WindowName(),
-                widget.Notify(),
+                widget.Prompt(),
                 widget.StatusNotifier(), # As systray is no available in wayland
                 widget.TextBox(**arrow_right),
                 widget.Net(font=mono_font, format= " {down:^5.1f}{down_suffix:<2}", background=colours["BLUE"],**arrow_right),  # blue
@@ -190,7 +163,7 @@ class MyBars:
                 widget.CurrentLayoutIcon(mouse_callbacks={"Button1": lazy.next_layout()}),
                 my_widgets.group_box(),
                 widget.WindowName(),
-                widget.Notify(),
+                widget.Prompt(),
                 widget.StatusNotifier(), # As systray is not available in wayland
                 widget.TextBox(**arrow_right),
                 my_widgets.wallpaper_switcher(**arrow_right),
@@ -221,6 +194,7 @@ class MyBars:
                 widget.CurrentLayoutIcon(),
                 my_widgets.group_box(),
                 widget.WindowName(),
+                widget.Prompt(),
                 widget.TextBox(**arrow_right),
                 widget.Clock(format="%d/%m/%Y %a %I:%M %p", background="1d6ac9", **arrow_right),   # Light blue
                 my_widgets.power_button(),
@@ -237,7 +211,8 @@ class MyBars:
                 widget.CurrentLayoutIcon(mouse_callbacks={"Button1": lazy.next_layout()}),
                 my_widgets.group_box(),
                 widget.WindowName(),
-                widget.Systray(),
+                widget.Prompt(),
+                widget.StatusNotifier(),
                 widget.TextBox(**arrow_right),
                 my_widgets.wallpaper_switcher(**arrow_right),
                 my_widgets.volume(**arrow_right),
@@ -257,6 +232,7 @@ class MyBars:
                 widget.CurrentLayoutIcon(mouse_callbacks={"Button1": lazy.next_layout()}),
                 my_widgets.group_box(),
                 widget.WindowName(),
+                widget.Prompt(),
                 widget.StatusNotifier(),
                 widget.TextBox(**arrow_right),
                 my_widgets.wallpaper_switcher(**arrow_right),
