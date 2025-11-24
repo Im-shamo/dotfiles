9ef3877bd3732db134a8070eb9ca04c8ee9c80f8
Im-shamo <samuelkwok2007@gmail.com>
2025-11-25 02:56:23 +0800

    Add install_yay.sh

---
+++ scripts/archlinux/yay_install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+sudo pacman -S --needed --noconfirm base-devel git
+
+if [[ $(pacman -Qq yay) != "yay" ]]; then
+    mkdir -p Clone
+    cd Clone
+    git clone https://aur.archlinux.org/yay.git
+
+    cd yay
+    makepkg -si --noconfirm
+else
+    echo yay is already installed.
+fi
