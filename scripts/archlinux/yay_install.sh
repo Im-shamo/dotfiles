8a6a7edffd4c76feb08f1df93497d43c6e25c6de
Im-shamo <samuelkwok2007@gmail.com>
2025-12-12 21:15:15 +0800

    Fix yay_install.sh

--- scripts/archlinux/yay_install.sh
+++ scripts/archlinux/yay_install.sh
@@ -4,7 +4,7 @@ sudo pacman -S --needed --noconfirm base-devel git
 
 if [[ $(pacman -Qq yay) != "yay" ]]; then
     mkdir -p ~/Clone
-    cd Clone
+    cd ~/Clone
     git clone https://aur.archlinux.org/yay.git
 
     cd yay
