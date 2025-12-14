145e218b92a6bfb88a0a0726d1766fdd0765a6c4
Im-shamo <samuelkwok2007@gmailc.om>
2025-12-14 20:05:13 +0800

    Add laptop/desktop detection bars qtile config

--- qtile/configuration/screens.py
+++ qtile/configuration/screens.py
@@ -6,30 +6,25 @@ from configuration.helper_functions import is_desktop, get_display_resolutions
 
 display_resolutions = get_display_resolutions(qtile)
 
-# TODO: Add wayland
 if qtile.core.name == "x11":
-    screens = [
-        Screen(top = my_bars.main_bar_x11_desktop()),
-    ]
+    if is_desktop():
+        screens = [
+            Screen(top = my_bars.main_bar_x11_desktop())
+        ]
+    else:
+        screens = [
+            Screen(top = my_bars.main_bar_x11())
+        ]
 
 elif qtile.core.name == "wayland":
-     if len(display_resolutions) == 0:
-         if is_desktop():
-             screens = [Screen(top = my_bars.main_bar_wayland_desktop())]
-         else:
-             screens = [Screen(top = my_bars.main_bar_wayland())]
-
-     for name, resolution in display_resolutions.items():
-         if resolution[0] > 1000:
-             if is_desktop:
-                 screens = [Screen(top = my_bars.main_bar_wayland_desktop())]
-             else:
-                 screens = [Screen(top = my_bars.main_bar_wayland())]
-         else:
-             if is_desktop:
-                 screens = [Screen(top = my_bars.main_bar_wayland_small_desktop())]
-             else:
-                 screens = [Screen(top = my_bars.main_bar_wayland_small)]
+    if is_desktop():
+        screens = [
+            Screen(top = my_bars.main_bar_wayland_desktop())
+        ]
+    else:
+        screens = [
+            Screen(top = my_bars.main_bar_wayland())
+        ]
 
 else:
     screens = []
