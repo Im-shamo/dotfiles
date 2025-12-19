d426d84af63719fe77153d8419cb1beb6f889cdb
Im-shamo <samuelkwok2007@gmail.com>
2025-12-19 17:13:13 +0800

    Move some environmant variables to config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,10 @@
 set fish_greeting
 oh-my-posh init fish --config ~/Clone/dotfiles/shamo.omp.jsonc| source
+
+fish_add_path --path ~/.config/rofi/applets/bin ~/.config/rofi/scripts
+set -gx SSH_SSH_SOCK $XDG_RUNTIME_DIR/gcr/ssh
+set -gx EDITOR nvim
+set -gx PAGER less
+set -gx VISUAL $EDITOR
+set -gx BROWSER brave
+set -gx XDG_MENU_PREFIX arch-
