ffb94ccd80154c91f05f3fc100d05d4c423423b6
Im-shamo <samuelkwok2007@gmail.com>
2025-12-29 04:22:01 +0800

    Updated config_linking and add .Xresources

--- scripts/archlinux/config_linking.sh
+++ scripts/archlinux/config_linking.sh
@@ -8,6 +8,8 @@ folders=(
     "picom" "qtile" "waybar" "wofi" "nvim"
     )
 
+files=(".vimrc" ".Xresources")
+
 if [[ ! -d $dots ]]; then
     echo "$dots folder does not exist."
     exit 1
@@ -21,21 +23,34 @@ fi
 cd $config
 
 for foldername in "${folders[@]}"; do
-    target="$dots"/"$foldername"
-    config_location="$config"/"$foldername"
+    target="$dots/$foldername"
+    config_location="$config/$foldername"
 
     # If the config file already exists, then rename it
-    if [[ -d "$config_location" ]] || [[ -f "$config_location" ]]; then
-        mv "$config_location" "${config_location}".bak
+    if ( [[ -d "$config_location" ]] || [[ -f "$config_location" ]] ) && [[ ! -h "$config_location" ]]; then
+        mv -v "$config_location" "${config_location}".bak
     fi
 
     if [[ -d "$target" ]] || [[ -f "$target" ]]; then
-        ln -s "$target"
-        echo "$config_location -> $target"
+        ln -sv "$target"
     else
         echo "error: $target does not exist!"
     fi
 done
 
 cd $HOME
-ln -s "$dots"/.vimrc .
+for filename in "${files[@]}"; do
+    target="$dots/$filename"
+    config_location="$HOME/$filename"
+
+    # If the config file already exists, then rename it
+    if ( [[ -d "$config_location" ]] || [[ -f "$config_location" ]] ) && [[ ! -h "$config_location" ]]; then
+        mv -v "$config_location" "${config_location}".bak
+    fi
+
+    if [[ -d "$target" ]] || [[ -f "$target" ]]; then
+        ln -sv "$target"
+    else
+        echo "error: $target does not exist!"
+    fi
+done
