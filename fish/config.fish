e07bbb56c2d12dbc4142caae51096c01f05343ab
Im-shamo <samuelkwok2007@gmailc.om>
2025-12-31 00:12:33 +0800

    Update fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
+test -e /usr/share/cachyos-fish-config/cachyos-config.fish && source /usr/share/cachyos-fish-config/cachyos-config.fish
 set fish_greeting
-oh-my-posh init fish --config ~/Clone/dotfiles/shamo.omp.jsonc| source
+test -e /usr/bin/oh-my-posh && oh-my-posh  init fish --config ~/Clone/dotfiles/shamo.omp.jsonc| source
 
 fish_add_path --path ~/.config/rofi/applets/bin ~/.config/rofi/scripts
 set -gx SSH_SSH_SOCK $XDG_RUNTIME_DIR/gcr/ssh
