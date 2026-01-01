7b22755b11ef27589180e64c67b630b5669704dc
Im-shamo <samuelkwok2007@gmail.com>
2026-01-02 01:19:54 +0800

    Update kitty font

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 test -e /usr/share/cachyos-fish-config/cachyos-config.fish && source /usr/share/cachyos-fish-config/cachyos-config.fish
 set fish_greeting
-test -e /usr/bin/oh-my-posh && oh-my-posh  init fish --config ~/Clone/dotfiles/shamo.omp.jsonc| source
+test -e /usr/bin/oh-my-posh && oh-my-posh init fish --config ~/Clone/dotfiles/shamo.omp.jsonc | source
 
 fish_add_path --path ~/.config/rofi/applets/bin ~/.config/rofi/scripts
 set -gx SSH_SSH_SOCK $XDG_RUNTIME_DIR/gcr/ssh
