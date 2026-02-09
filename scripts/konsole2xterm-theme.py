81531d2a9011f65fe67cc9ed801e667d1b55d1c5
Im-shamo <samuelkwok2007@gmail.com>
2026-02-09 09:22:39 +0800

    Add konsole2xterm.py script

---
+++ scripts/konsole2xterm-theme.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import configparser
+import argparse
+from pathlib import Path
+
+parser = argparse.ArgumentParser("Converts Konsole theme to xterm theme")
+parser.add_argument("theme", help="File path of theme")
+args = parser.parse_args()
+filepath = Path(args.theme).expanduser()
+
+with open(filepath) as f:
+    conf = configparser.ConfigParser()
+    conf.read_file(f)
+
+s = {
+    "Background": "background",
+    "Foreground": "foreground"
+}
+
+color_list = [
+    "Black",
+    "Red",
+    "Green",
+    "Yellow",
+    "Blue",
+    "Magenta",
+    "Cyan",
+    "White"
+]
+
+for i in range(0,8):
+    s["Color" + str(i)] = "color" + str(i)
+    s["Color" + str(i) + "Intense"] = "color" + str(i+8)
+
+output = []
+i = 0
+for key,value in s.items():
+    tmp = conf.get(key, "Color")
+    tmp = tmp.split(",")
+    tmp = [hex(int(tmp[0]))[2:], hex(int(tmp[1]))[2:], hex(int(tmp[2]))[2:]]
+    output.append(f"*{value}: rgb: {'/'.join(tmp)}")
+
+print("\n".join(output))
