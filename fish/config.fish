b440ac2304cd460b2698baf15eac2a3ce00fd77b
Im-shamo <samuelkwok2007@gmail.com>
2026-02-13 23:22:49 +0800

    Update fish
    add lesspipe

--- fish/config.fish
+++ fish/config.fish
@@ -3,9 +3,11 @@ set fish_greeting
 test -e /usr/bin/oh-my-posh && oh-my-posh init fish --config ~/Clone/dotfiles/shamo.omp.jsonc | source
 
 fish_add_path --path ~/.config/rofi/applets/bin ~/.config/rofi/scripts
-set -gx SSH_SSH_SOCK $XDG_RUNTIME_DIR/gcr/ssh
-set -gx EDITOR vim
-set -gx PAGER less
-set -gx VISUAL $EDITOR
-set -gx BROWSER brave
-set -gx XDG_MENU_PREFIX arch-
+set -gx SSH_SSH_SOCK "$XDG_RUNTIME_DIR/gcr/ssh"
+set -gx EDITOR "vim"
+set -gx PAGER "less"
+set -gx LESSOPEN "| /usr/bin/src-hilite-lesspipe.sh %s"
+set -gx LESS " -R"
+set -gx VISUAL "$EDITOR"
+set -gx BROWSER "brave"
+set -gx XDG_MENU_PREFIX "arch-"
